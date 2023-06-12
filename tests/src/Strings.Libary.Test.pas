8dab8a1f6838621d995f41860c7d5ec8347bfebc
matheusbarzon <matheusbarzon@gmail.com>
2023-06-12 08:00:57 -0300

    Adicionado README em ingles e feito teste da funcoes de abreviacoes

--- tests/src/Strings.Libary.Test.pas
+++ tests/src/Strings.Libary.Test.pas
@@ -31,6 +31,21 @@ type
     procedure ShouldKeepSelectedChars(AInputString, AKeepChars,
       AOutputString: String);
 
+    [TestCase('Should abbreviate "AVENIDA"', 'AVENIDA JOAO LUCAS FILHO,14,AV J. L. FILHO')]
+    [TestCase('Should abbreviate "RUA"', 'rua maria filha dos santos,21,R M. F. DOS SANTOS')]
+    [TestCase('Should abbreviate "CONDOMINIO"', 'CONDOMINIO RAFAEL LIMA SOBRINHO,19,COND R. L. SOBRINHO')]
+    [TestCase('Should abbreviate "CONDOMÍNIO"', 'CONDOMÍNIO MARCIA SOUZA SOBRINHA,19,COND M. S. SOBRINHA')]
+    [TestCase('Should abbreviate "BLOCO"', 'bloco marcelo claudio,16,BL M. CLAUDIO')]
+    [TestCase('Should abbreviate "APARTAMENTO"', 'apartamento maria dos santos,18,APTO M. DOS SANTOS')]
+    [TestCase('Should abbreviate "PRACA"', 'PRACA LEONARDO ARRANTES NETO,17,PC L. A. NETO')]
+    [TestCase('Should abbreviate "PRAÇA"', 'PRAÇA JESSICA MENEQUEL NETA,18,PC J. M. NETA')]
+    [TestCase('Should abbreviate "JARDIM"', 'JARDIM CLARICE JOANA MOREIRA,16,JD C. J. MOREIRA')]
+    [TestCase('Should abbreviate "EDIFICIO"', 'EDIFICIO EMILY MARLENE MENDES,15,ED E. M. MENDES')]
+    [TestCase('Should abbreviate "CORONEL"', 'CORONEL IAN ARTHUR PAULO,16,CRN IAN A. PAULO')]
+    [TestCase('Should abbreviate "EXPEDICIONARIO"', 'EXPEDICIONARIO BERNARDO JOSÉ LUAN FARIAS,19,EXP B. J. L. FARIAS')]
+    [TestCase('Should abbreviate "RODOVIA"', 'RODOVIA RAFAEL VICENTE DA ROSA,17,ROD R. V. DA ROSA')]
+    procedure ShouldAbbreviateAddress(AInputString:String; ALength: Integer; AOutputString: String);
+
     [TestCase('Should abbreviate "Filho"', 'JOAO LUCAS FILHO,10,JOAO L. FH')]
     [TestCase('Should abbreviate "Filha"', 'maria filha dos santos,19,MARIA FH DOS SANTOS')]
     [TestCase('Should abbreviate "Sobrinho"', 'RAFAEL LIMA SOBRINHO,14,RAFAEL L. SB')]
@@ -40,7 +55,6 @@ type
     [TestCase('Should abbreviate "Neto"', 'LEONARDO ARRANTES NETO,14,LEONARDO A. NT')]
     [TestCase('Should abbreviate "Neta"', 'JESSICA MENEQUEL NETA,15,JESSICA M. NT')]
     procedure ShouldAbbreviateName(AInputString:String; ALength: Integer; AOutputString: String);
-
 
   end;
 
@@ -71,6 +85,18 @@ var
 begin
   { Arrange } { Act }
   LStringFromFunction := TStringUtil.OnlyChars(AInputString);
+
+  { Assert }
+  Assert.AreEqual(AOutputString, LStringFromFunction);
+end;
+
+procedure TTestStringsLibrary.ShouldAbbreviateAddress(AInputString: String;
+  ALength: Integer; AOutputString: String);
+var
+  LStringFromFunction: String;
+begin
+  { Arrange } { Act }
+  LStringFromFunction := TStringUtil.AbbreviateAddress(AInputString, ALength);
 
   { Assert }
   Assert.AreEqual(AOutputString, LStringFromFunction);
